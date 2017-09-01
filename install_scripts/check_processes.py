05dd5926c7996d27f58ac6eb232cafb98aae6327
submeter <submeter@submeter1.local>
2017-09-01 16:00:39 +0100

    add more warning sounds

--- install_scripts/check_processes.py
+++ install_scripts/check_processes.py
@@ -10,6 +10,7 @@ SCRIPT_NAME = 'rfm_ecomanager_logger.py'
 EXE_NAME = 'python'
 FAILED_FILE = os.path.join(os.environ['HOME'], '__check_lock__')
 AUDIO_WARNING = '/usr/share/sounds/speech-dispatcher/test.wav'
+AUDIO_TEST = '/usr/share/sounds/speech-displatcher/dummy-message.wav'
 LOG_FILE = os.path.join(os.environ['HOME'], 'check.log')
 
 
@@ -25,17 +26,21 @@ def check_process_running():
     return None
 
 
-def play_warning():
+def play_sound(audio_file, n_warn=1):
     logging.info("Playing warning @ %s" % datetime.now())
-    subprocess.Popen(['paplay', '--volume', '65536', AUDIO_WARNING])
+    # To get the sound card working!
+    subprocess.call(['/usr/sbin/alsactl', 'init'])
+    for i in range(n_warn):
+        # Blocking calls to ensure a single audio warning at a time
+        subprocess.call(['paplay', '--volume', '65536', audio_file])
 
-
+    
 try:
     process_running = check_process_running()
 except:
     # Unable to check the process! WARN!
     logging.exception('Failed to check processes')
-    play_warning()
+    play_sound(AUDIO_WARNING, 3)
 else:
     if process_running:
         if os.path.exists(FAILED_FILE):
@@ -46,6 +51,6 @@ else:
         subprocess.Popen(cmd.split(), cwd=os.path.join(os.environ['HOME'],
                                                        'rfm_ecomanager_logger'))
         if os.path.exists(FAILED_FILE):
-            play_warning()
+            play_sound(AUDIO_WARNING, 3)
         with open(FAILED_FILE, 'w') as f:
             f.write('FAILED AT %s' % datetime.now())
